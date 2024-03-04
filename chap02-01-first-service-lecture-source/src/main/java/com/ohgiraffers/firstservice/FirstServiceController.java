d062aa4b66d542d5b443c28852509911b1424115
alu98@naver.com <alu98@naver.com>
2024-03-04 12:24:55 +0900

    lb를 통한 port변경 확인

--- chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
+++ chap02-01-first-service-lecture-source/src/main/java/com/ohgiraffers/firstservice/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+
+/* 설명. Gateway 추가 후 Routes 경로 추가 후 작성했던 부분 */
 //@RequestMapping("/first-service")
+
+/* 설명. Gateway에서 RewritePath 필터 추가 후 뒤에 segment만 넘어오게 한 이후 */
 @RequestMapping("/")
 public class FirstServiceController {
 
-    @GetMapping("/health_check")
-    public String healthCheck() {
-        return "난 괜차나";
+    /* 설명. @Value와 같이 application.yml 또는 환경설정 값 불러오기 위해 추가(생성자 주입) */
+    private Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
     }
 
+    @GetMapping("/health_check")
+    public String healthCheck() {
+
+        /* 설명. first-service application(포트) 여러개 실행 시(스케일 아웃) 실행되는 application 확인(포트 번호로) */
+        /* 설명. Gateway의 lb(로드 밸런싱)를 통해 RoundRobin 방식으로 스위칭됨을 확인 */
+        return "난 괜차나.." + env.getProperty("local.server.port");
+    }
+
+    /* 설명. Gateway를 거치고 넘어오는 요청 시 RequestHeader에 값이 추가됨(Gateway의 필터에 의해) */
     @GetMapping("/message")
     public String message(@RequestHeader("first-request") String header) {
         log.info("넘어온 헤더값: {}", header);

7738aac4341c14abf0dca3d2bcb2a39ec38fe460
alu98@naver.com <alu98@naver.com>
2024-03-06 12:14:16 +0900

    user-service/users/{userId} 조회까지 완료

--- chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserServiceImpl.java
+++ chap04-01-user-service-lecture-source/src/main/java/com/ohgiraffers/userservice/service/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ohgiraffers.userservice.service;
 
 import com.ohgiraffers.userservice.aggregate.UserEntity;
+import com.ohgiraffers.userservice.client.OrderServiceClient;
 import com.ohgiraffers.userservice.dto.UserDTO;
 import com.ohgiraffers.userservice.repository.UserRepository;
+import com.ohgiraffers.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
@@ -15,23 +17,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
 
-    private UserRepository userRepository;
-    private ModelMapper modelMapper;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    /* 설명. FeignClient 이후 추가할 부분 */
+    private OrderServiceClient orderServiceClient;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository,
                            ModelMapper modelMapper,
-                           BCryptPasswordEncoder bCryptPasswordEncoder) {
+                           BCryptPasswordEncoder bCryptPasswordEncoder,
+                           OrderServiceClient orderServiceClient) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.orderServiceClient = orderServiceClient;
     }
 
     @Transactional
@@ -84,10 +92,15 @@ public class UserServiceImpl implements UserService{
     @Override
     public UserDTO getUserById(String id) {
         UserEntity userEntity = userRepository.findById(Long.valueOf(id))
-                .orElseThrow(() -> {return new UsernameNotFoundException("조회된 회원 없음");
+                .orElseThrow(() -> { return new UsernameNotFoundException("조회된 회원 없음");
                 });
 
         UserDTO userDTO = modelMapper.map(userEntity, UserDTO.class);
+
+        /* 설명. FeignClient를 통한 통신 시 추가할 부분 */
+        List<ResponseOrder> orderList = orderServiceClient.getUserOrders(id);
+
+        userDTO.setOrders(orderList);
 
         return userDTO;
     }
